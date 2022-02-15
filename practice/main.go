65dc55f21d1042ab567bcf86ff3e8858c52b0a04
Leslie <75267383+lescardone@users.noreply.github.com>
2022-02-15 10:02:02 -0700

    notes

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bufio" //for input
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	var aString string = "This is Go!"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
+	/* 	var aString string = "This is Go!"
+	   	fmt.Println(aString)
+	   	fmt.Printf("The variable's type is %T", aString) */
+	// %T	a Go-syntax representation of the type of the value
 
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	// characters wrapped in single quotes
+	// strings wrappedin double quotes
+	fmt.Println("You entered:", input)
 }
