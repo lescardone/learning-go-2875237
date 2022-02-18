b5cb2ed6b68da0b9b89c827cec6eaad23f2dd4d1
Leslie <75267383+lescardone@users.noreply.github.com>
2022-02-18 11:04:54 -0700

    Leslie: notes

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Println("I am printing this at ", n)
+
+	t := time.Date(2022, time.February, 15, 10, 50, 0, 0, time.UTC)
+	fmt.Println("Go launched at ", t)
+	fmt.Println(t.Format(time.ANSIC))
 
 }
